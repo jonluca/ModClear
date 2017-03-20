78102b05683395b26d7d98aeecaf5260392af929
JonLuca DeCaro <jdecaro@usc.edu>
2017-03-20 11:29:29 -0700

    Partial completion

---
+++ content_script.js
@@ -0,0 +1,14 @@
+function clearNotifs() {
+    var archiveIcons = $(".ThreadPreview__controlIcon");
+
+    for (icon in archiveIcons) {
+        archiveIcons.click();
+    }
+    console.log('running');
+}
+
+var topBar = $(".ThreadPreviewViewerHeader");
+topBar.append("<img id=\"clearAllInjected\" class=\"ThreadPreviewViewerHeader__icon\" src=" + chrome.extension.getURL('images/insert.png') + ">");
+
+$("#clearAllInjected").on('click', clearNotifs);
+document.getElementById("clearAllInjected").style.cursor = "pointer";
---
+++ js/background.js
@@ -0,0 +1,8 @@
+﻿chrome.browserAction.onClicked.addListener(function() { //Fired when User Clicks ICON
+    var archiveIcons = $(".ThreadPreview__controlIcon");
+
+    for (icon in archiveIcons) {
+        archiveIcons.click();
+    }
+    console.log('running');
+});
