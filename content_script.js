94e683a5f499d57c79167278c643c8b36f0ced49
JonLuca DeCaro <jdecaro@usc.edu>
2017-03-20 16:33:15 -0700

    Images, description

--- content_script.js
+++ content_script.js
@@ -4,11 +4,27 @@ function clearNotifs() {
     for (icon in archiveIcons) {
         archiveIcons.click();
     }
-    console.log('running');
+    while ($(".ThreadPreview__controlIcon").length != 0) {
+        var archiveIcons = $(".ThreadPreview__controlIcon");
+
+        for (icon in archiveIcons) {
+            archiveIcons.click();
+        }
+        console.log('running');
+    }
 }
 
 var topBar = $(".ThreadPreviewViewerHeader");
 topBar.append("<img id=\"clearAllInjected\" class=\"ThreadPreviewViewerHeader__icon\" src=" + chrome.extension.getURL('images/icon.png') + ">");
 
 $("#clearAllInjected").on('click', clearNotifs);
+$("#clearAllInjected").css('margin-right', '10px');
+$("#clearAllInjected").css('margin-top', '10px');
+$("#clearAllInjected").css('float', 'right');
+$("#clearAllInjected").hover(function() {
+    $("#clearAllInjected").attr("src", chrome.extension.getURL('images/hover.png'));
+}, function() {
+    $("#clearAllInjected").attr("src", chrome.extension.getURL('images/icon.png'));
+});
+
 document.getElementById("clearAllInjected").style.cursor = "pointer";
